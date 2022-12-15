1ce6feaa36d9dca7cdf62f9b882f7721f5bb552a
Imtua <itsololevelling@gmail.com>
2022-12-15 17:16:15 +0300

    Добавьте файлы проекта.

---
+++ Module3App/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter your name: ");
+string myName = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+int myAge = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Your name is {myName} and age is {myAge}");
+
+Console.Write("Enter your birthdate: ");
+string myDate = Console.ReadLine();
+myDate = DateTime.Parse(myDate).ToShortDateString();
+Console.WriteLine($"Your birthdate is {myDate}");
+
+
